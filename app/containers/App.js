69abfb670906080a43d62c51eecaf05b67f5b6e1
Jean-Marie Cart-Lamy <jean-marie.cart-lamy@fullsix.com>
2017-01-20 14:47:44 +0100

    Add example Reducer

--- app/containers/App.js
+++ app/containers/App.js
@@ -5,8 +5,22 @@ import { Text, View } from 'react-native';
 
 import styles from '../styles.js';
 
-export default class Index extends Component {
+/*
+  Example with reducer / action :
+
+  import { bindActionCreators } from 'redux';
+  import { connect } from 'react-redux';
+  import * as exampleActions from '../actions/exampleActions.js';
+ */
+
+class App extends Component {
   render() {
+
+    /*
+      Check your props :
+      this.props.actions.userInput(value);
+     */
+
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
@@ -21,3 +35,26 @@ export default class Index extends Component {
     );
   }
 }
+
+/*
+  Connect your reducer and add state & dispatch in your props :
+
+  const mapStateToProps = (state) => {
+    return {
+      userInput: state.userInputReducer
+    }
+  };
+
+  const mapDispatchToProps = (dispatch) => {
+    return {
+      actions: bindActionCreators(exampleActions, dispatch)
+    }
+  };
+
+  export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+  )(App);
+ */
+
+export default App;
